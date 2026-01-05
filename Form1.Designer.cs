4b331cd430b4c668640973e39d26a9fa1035ea84
NemethAdam01 <nemeth.adam@diak.szbi-pg.hu>
2026-01-05 09:34:11 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace NA_ManualSoft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pdfReader = new AxAcroPDFLib.AxAcroPDF();
+            this.cb_selector = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pdfReader)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pdfReader
+            // 
+            this.pdfReader.Enabled = true;
+            this.pdfReader.Location = new System.Drawing.Point(342, 159);
+            this.pdfReader.Name = "pdfReader";
+            this.pdfReader.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("pdfReader.OcxState")));
+            this.pdfReader.Size = new System.Drawing.Size(1450, 770);
+            this.pdfReader.TabIndex = 0;
+            // 
+            // cb_selector
+            // 
+            this.cb_selector.FormattingEnabled = true;
+            this.cb_selector.Location = new System.Drawing.Point(26, 61);
+            this.cb_selector.Name = "cb_selector";
+            this.cb_selector.Size = new System.Drawing.Size(215, 21);
+            this.cb_selector.TabIndex = 1;
+            this.cb_selector.SelectedIndexChanged += new System.EventHandler(this.cb_selector_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fájl kiválasztás:";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(342, 58);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(110, 23);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "Fájl feltöltés";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1804, 941);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb_selector);
+            this.Controls.Add(this.pdfReader);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pdfReader)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxAcroPDFLib.AxAcroPDF pdfReader;
+        private System.Windows.Forms.ComboBox cb_selector;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpload;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NA_ManualSoft
+{
+    public partial class Form1 : Form
+    {
+        FileInfo[] filesNames;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\NemethAdam\Desktop\NA_ManualSoft\NA_ManualSoft\bin\Debug\manuals"); 
+
+            filesNames = d.GetFiles("*.pdf"); 
+            string str = "";
+
+            foreach (FileInfo file in filesNames)
+            {
+                str = str + ", " + file.Name;
+            }
+            foreach (FileInfo file in filesNames)
+            {
+                cb_selector.Items.Add(file.Name);
+            }
+            pdfReader.LoadFile("manual_monitor.pdf");
+
+
+        }
+
+        private void cb_selector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pdfReader.LoadFile(filesNames[cb_selector.SelectedIndex].FullName);
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            string saveDirectory = @"C:\Users\NemethAdam\Desktop\NA_ManualSoft\NA_ManualSoft\bin\Debug\manuals";
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (!Directory.Exists(saveDirectory))
+                    {
+                        Directory.CreateDirectory(saveDirectory);
+                    }
+
+                    string fileName = Path.GetFileName(openFileDialog1.FileName);
+                    string fileSavePath = Path.Combine(saveDirectory, fileName);
+                    File.Copy(openFileDialog1.FileName, fileSavePath, true);
+                    cb_selector.Items.Add(fileName);
+                }
+            }
+        }
+    }
+}
